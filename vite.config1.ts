a8d2c145e7b985c8e0a4305f8f849848d269dc12
Elijah-Christian Wiegand <ewiegand51@gmail.com>
2024-03-15 10:58:54 -0500

    updated html file and config file

--- vite.config1.ts
+++ vite.config1.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ecdub27.github.io/ECdub27.io/'
+  base: '/',
 })
