670843482845169bb048da853b90b73c8e120c7c
Elijah-Christian Wiegand <ewiegand51@gmail.com>
2024-03-18 09:05:50 -0500

    testing new api

--- src/Components/Header/hero.tsx
+++ src/Components/Header/hero.tsx
@@ -11,11 +11,11 @@ function HeroSlide() {
   return (
     <div>
     <Box>
-    <BasicTimeline key={''} message={''} count={0} disabled={false} names={[]} status={'waiting'} description={''} title={''} name={''}  position={''} urlToImage={''} rowHeight={0} flexItems={0} href={''} onClick={function (): void {
+    <BasicTimeline key={''} message={''} count={0} disabled={false} names={[]} status={'waiting'} description={''} title={''} name={''} position={''} urlToImage={''} rowHeight={0} flexItems={0} href={''} onClick={function (): void {
           throw new Error('Function not implemented.');
         } } setState={function (): void {
           throw new Error('Function not implemented.');
-        } } />
+        } } image={undefined} />
     </Box>
     </div>
   );
