531c0fc3e6aa16601a324f4afa5287bcaeddb63c
Elijah-Christian Wiegand <ewiegand51@gmail.com>
2024-03-26 21:25:32 -0500

    when will my env file ever work for me?

---
+++ Client/src/Components/Header/hero.tsx
@@ -0,0 +1,27 @@
+
+import BasicTimeline from './timeline';
+import Box from '@mui/material/Box';
+
+
+
+
+
+
+function HeroSlide() {
+ 
+
+  return (
+    <div>
+    <Box>
+    <BasicTimeline key={''} message={''} count={0} disabled={false} names={[]} status={'waiting'} description={''} title={''} name={''} position={''} urlToImage={''} rowHeight={0} flexItems={0} href={''} onClick={function (): void {
+          throw new Error('Function not implemented.');
+        } } setState={function (): void {
+          throw new Error('Function not implemented.');
+        } } image={undefined} />
+       
+    </Box>
+    </div>
+  );
+}
+
+export default HeroSlide;
