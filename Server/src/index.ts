d9cb76809b1b4b464d65b129aba2d447f76e7197
Elijah-Christian Wiegand <ewiegand51@gmail.com>
2024-03-27 10:44:18 -0500

    start server

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,41 +1,46 @@
-
 import express from 'express';
 import cors from 'cors';
+import { config } from 'dotenv';
+import path from 'path';
 import asyncHandler from 'express-async-handler';
-import 'dotenv/config';
+config({ path: path.resolve(__dirname, 'protected.env') });
+const port = process.env.PORT || 4000;
+console.log(process.env.PORT);
+
+
+
+
 
 const app = express();
-const port = process.env.PORT || 4000;
+
 app.use(cors());
-
-
 app.options('*', cors());
 
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+res.send('Hello World');
 });
 
-app.get('/api', (req, res) => { 
+app.get('/api/', asyncHandler(async(req, res) => { 
 
-});
+}));
 
 
-app.get('/api', (req, res) => { 
+app.get('/api', asyncHandler(async(req, res) => { 
 
-});
+}));
 
-app.get('/api', (req, res) => { 
+app.get('/api', asyncHandler(async (req, res) => { 
 
-});
+}));
 
-app.get('/api', (req, res) => { 
+app.get('/api', asyncHandler(async(req, res) => { 
 
-});
+}));
 
-app.get('/api', (req, res) => { 
+app.get('/api', asyncHandler(async (req, res) => { 
 
-});
+}));
 
 
 
